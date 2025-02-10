7bb9efc5ea42a983de9affbede5da04ab307060f
aexiting <adamttway@gmail.com>
2025-02-09 21:17:58 -0500

    started working on creating a reducer for to do list and updated eslint

--- src/components/todo-list/use-todo-list-state.tsx
+++ src/components/todo-list/use-todo-list-state.tsx
@@ -1,29 +1,39 @@
 import {useReducer} from "react";
 
-type Task = {
+export type Task = {
     title: string;
     details: string;
     isDone: boolean;
 }
 
-type TodoListStateProps = {
+export type TodoListStateProps = {
     maxTasks: number;
     listOfTasks?: Array<Task>;
 }
 
-type TodoListState = {
+export type TodoListState = {
     listOfTasks: Array<Task>;
 }
 
-type TodoListActions = {
-
+export type TodoListActions = {
+    updateTask: (task: Task) => void;
+    deleteTask: (task: Task) => void;
+    createTask: (task: Task) => void;
+    setIsDone: (isDone: boolean, task: Task) => void;
 }
+type Action =
+    {type: 'UpdateTask'; payload: Task} |
+    {type: 'DeleteTask'; payload: Task} |
+    {type: 'CreateTask'; payload: Task} |
+    {type: 'TaskIsDoneSet'; payload: Task};
 
 const useTodoListState = ({ maxTasks, listOfTasks }: TodoListStateProps) => {
 
-    const initialTasks =
-    const tasksReducer = () => {
+    const initialState = {
+        listOfTasks: listOfTasks ?? []
+    }
+    const tasksReducer: React.Reducer<TodoListState, Action> = (toDoListState: TodoListState, actions) => {
 
     }
-    const [tasks, dispatch] = useReducer(tasksReducer, initialTasks)
+    const [tasks, dispatch] = useReducer(initialState)
 }
